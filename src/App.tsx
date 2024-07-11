67d163158a50441f9883e982ea4d89540161d5f3
mxrsel <marsel14082006@gmail.com>
2024-07-11 14:55:25 +0600

    added styles and html on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import DoorPassword from './containers/DoorPassword/DoorPassword.tsx';
 
 function App() {
   return (
     <div className="App">
-
+      <DoorPassword />
     </div>
   )
 }
--- src/containers/DoorPassword/DoorPassword.tsx
+++ src/containers/DoorPassword/DoorPassword.tsx
@@ -1,8 +1,32 @@
-
+import '../DoorPassword/DoorPassword.css'
 const DoorPassword = () => {
   return (
-    <div>
 
+    <div className="door-password">
+      <div className="container top-three">
+        <button className="btn-password">1</button>
+        <button className="btn-password">2</button>
+        <button className="btn-password">3</button>
+      </div>
+
+      <div className=" container second-three">
+        <button className="btn-password">4</button>
+        <button className="btn-password">5</button>
+        <button className="btn-password">6</button>
+      </div>
+
+      <div className="container hird-three">
+        <button className="btn-password">7</button>
+        <button className="btn-password">8</button>
+        <button className="btn-password">9</button>
+      </div>
+
+      <div className="container actions">
+        <button className="btn-password">{'<'}</button>
+        <button className="btn-password">0</button>
+        <button className="btn-password">E</button>
+      </div>
+      <button>Reset</button>
     </div>
   );
 };
